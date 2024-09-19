8fe5fb19ea76ba72aa1a200288d8529afa0a7bfd
65ani <anitha.nhk26@gmail.com>
2024-09-19 16:01:15 +0530

    react project1

---
+++ word-letter-counter/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+
+
+import WordLetterCounter from "./reportWebVitals.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id="top">
+        Mini project1
+      </h1>
+      <h1>
+        words and letters
+        Counters
+      </h1>
+      <WordLetterCounter/>
+    </div>
+  );
+}
+
+export default App;
