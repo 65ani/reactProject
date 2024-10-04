f2ee1d03e24d796bd6609f14f4fdf0ce090622ba
65ani <anitha.nhk26@gmail.com>
2024-10-04 09:57:16 +0000

    finished read operation

--- react-router-lab/src/components/PersonList.js
+++ react-router-lab/src/components/PersonList.js
@@ -1,86 +1,65 @@
-import React from 'react';
-//import personadd from './PersonAdd"
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 //import notification from 
+
+import Notification from './Notification';
+import '../styles/PersonList.css'; // Component-specific styles
+
 const API_URL=process.env.REACT_APP_API_URL
+
 const output = axios.get("https://5000-65ani-reactproject-2vazi3aj9yz.ws-us116.gitpod.io/persons");
 
-
 const PersonList = () => {
-  console.log(output);
-  console.log(API_URL)
+  const[people, setPeople] = useState([]);
+  const[notification, setNotification] =useState('');
 
-    return(
-        <div>
-            <h2>to add list of  the person</h2>
-        </div>
-    )
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        console.log(response);
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Error fetching people:', error);
+      }
+    };
+    fetchPeople();
+  }, []);
 
-}
+  return (
+    <div className="person-list">
+      <h1>Person List</h1>
+      <Link to="/add" className="btn btn-add add-person-button">Add Person</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map(person => (
+            <tr key={person.id}>
+              <td>
+                <Link to={`/person/${person.id}`} className="person-name">
+                  {person.name}
+                </Link>
+              </td>
+              <td>{person.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {notification && (
+        <Notification message={notification} onClose={() => setNotification('')} />
+      )}
+    </div>
+  );
+};
+
 export default PersonList;
 
 
 
 
-// // src/components/PersonList.js
-
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-
-// import Notification from './Notification';
-// import '../styles/PersonList.css'; // Component-specific styles
-
-// const API_URL = process.env.REACT_APP_API_URL;
-// console.log(API_URL )
-
-// const PersonList = () => {
-//   const [people, setPeople] = useState([]);
-//   const [notification, setNotification] = useState('');
-
-//   useEffect(() => {
-//     const fetchPeople = async () => {
-//       try {
-//         const response = await axios.get(API_URL);
-//         console.log(response);
-//         setPeople(response.data);
-//       } catch (error) {
-//         console.error('Error fetching people:', error);
-//       }
-//     };
-//     fetchPeople();
-//   }, []);
-
-//   return (
-//     <div className="person-list">
-//       <h1>Person List</h1>
-//       <Link to="/add" className="btn btn-add add-person-button">Add Person</Link>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Age</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {people.map(person => (
-//             <tr key={person.id}>
-//               <td>
-//                 <Link to={`/person/${person.id}`} className="person-name">
-//                   {person.name}
-//                 </Link>
-//               </td>
-//               <td>{person.age}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       {notification && (
-//         <Notification message={notification} onClose={() => setNotification('')} />
-//       )}
-//     </div>
-//   );
-// };
-
-// }
-// export default PersonList;
